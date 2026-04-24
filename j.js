e4e679263694c64e9b6bb35aae32f001b552d66e
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-04-24 17:00:17 -0600

    Added arithmetic functions.
    No UI, just basic operational functionality.

---
+++ j.js
@@ -0,0 +1,31 @@
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+const operate = function (a, op, b) {
+  if (op == "+") {
+    add(a, b);
+  } else if (op == "-") {
+    subtract(a, b);
+  } else if (op == "*") {
+    multiply(a, b);
+  } else if (op == "/") {
+    divide(a, b);
+  }
+};
+
+let num1;
+let num2;
+let operator;
